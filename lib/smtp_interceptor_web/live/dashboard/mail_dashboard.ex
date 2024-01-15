c0d1371b255da648c074c55b1621c9908937abff
fernando.basello <>
2024-01-14 21:44:55 -0300

    html mail preview

--- lib/smtp_interceptor_web/live/dashboard/mail_dashboard.ex
+++ lib/smtp_interceptor_web/live/dashboard/mail_dashboard.ex
@@ -8,7 +8,8 @@ defmodule SmtpInterceptorWeb.MailDashboard do
     {:ok, 
       assign(socket, brightness: 10, 
                 form: to_form(%{"search"=>""}),
-                mails: [] 
+                mails: [],
+                mail_src: nil
                 ), 
       layout: false
     }
@@ -18,7 +19,7 @@ defmodule SmtpInterceptorWeb.MailDashboard do
   def render(assigns) do
     ~H"""
     <div class="flex w-screen p-4 h-screen divide-x divide-gray-200">
-      <div class="flex flex-col basis-2/4 pl-4 divide-y-2 divide-gray-200">
+      <div class="flex flex-col basis-1/4 pl-4 divide-y-2 divide-gray-200">
         <div class="pb-2">
           <.search_input title="Cuenta" form={@form}/>
         </div>
@@ -36,6 +37,7 @@ defmodule SmtpInterceptorWeb.MailDashboard do
         </div>
       </div>
       <div class="flex basis-3/4 bg">
+        <iframe :if={@mail_src} class="flex w-full" src={"/dev/mailbox/#{@mail_src}/html"}/>
       </div>
     </div>
     """
@@ -45,24 +47,9 @@ defmodule SmtpInterceptorWeb.MailDashboard do
     {:noreply, assign(socket, form: to_form(%{"search" => ""}),
       mails: Mailer.search_by_account(search))}
   end
-  # handle_event
-  def handle_event("inc", _, socket) do
-    case socket.assigns.brightness do
-      100 -> {:noreply, socket}
-      _ -> {:noreply, 
-            assign(socket, brightness: socket.assigns.brightness + 10)}
-    end
-  end
-  
-  def handle_event("dec", _, socket) do
-    case socket.assigns.brightness do
-      0 -> {:noreply, socket}
-      _ -> {:noreply, assign(socket, brightness: socket.assigns.brightness - 10)}
-    end
-  end
 
-  def handle_event("select_mail", params, socket) do
-    IO.inspect(params)
-    {:noreply, socket}
+  # handle_event
+  def handle_event("select_mail", %{"email" => email_id}, socket) do
+    {:noreply, assign(socket, mail_src: email_id)}
   end
 end
